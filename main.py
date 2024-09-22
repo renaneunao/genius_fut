0ce19a652d0e54bb01774b1767f9232199adc645
Renan Barbosa <renan_vianna7@icloud.com>
2024-09-22 18:44:53 -0300

    Correção no cookie logged_in 1.3

--- main.py
+++ main.py
@@ -8,17 +8,6 @@ from dotenv import load_dotenv
 import sqlite3
 from datetime import datetime
 
-# Inicializa o controlador de cookies
-controller = CookieController()
-
-while True:
-    try:
-        logged_in = controller.get('logged_in')
-        break
-    except:
-        logged_in = False
-        break
-
 def criar_tabelas():
     # Conecte-se aos bancos de dados usando os caminhos absolutos
     conn_credenciais = sqlite3.connect('credenciais.db')
@@ -701,6 +690,10 @@ def main_page(controller):
 
 
 def main():
+    # Inicializa o controlador de cookies
+    controller = CookieController()
+
+    # Verifica se o usuário está logado
     logged_in = controller.get('logged_in')
 
     print(f'Main: O logged_in é: {logged_in}')
@@ -718,8 +711,7 @@ def main():
             criar_nova_conta()
     else:
         st.write('Primeiro login?')
-        controller.set('logged_in', False)
-        st.rerun()
+
 
 if __name__ == "__main__":
     main()
