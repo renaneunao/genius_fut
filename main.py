5bdcdeabfc683b21b88eec5219ff43a8e515e6a6
Renan Barbosa <139006953+renaneunao@users.noreply.github.com>
2024-10-02 12:29:04 -0300

    Update main.py
    test edicao botao 2

--- main.py
+++ main.py
@@ -996,8 +996,8 @@ def main_page(controller):
             st.markdown(
                 """
                 <style>
-                /* Seletor CSS do botão com base na largura */
-                button[style="width: 86.2969px"] {
+                /* Seletor CSS do botão fornecido */
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section.stMain.st-emotion-cache-bm2z3a.ea3mdgi8 > div.stBottom.st-emotion-cache-1p2n2i4.ea3mdgi7 > div > div > div > div > div > div.st-emotion-cache-0.e1f1d6gn0 > div > div > div.stHorizontalBlock.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.stColumn.st-emotion-cache-1b2d4l5.e1f1d6gn3 > div > div > div > div > div button {
                     background-color: black;  /* Fundo preto */
                     border: 2px solid #33CC00;  /* Define a borda verde neon */
                     border-radius: 5px;  /* Arredonda as bordas, se desejado */
@@ -1008,7 +1008,7 @@ def main_page(controller):
                 }
             
                 /* Efeito ao passar o mouse */
-                button[style="width: 86.2969px"]:hover {
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section.stMain.st-emotion-cache-bm2z3a.ea3mdgi8 > div.stBottom.st-emotion-cache-1p2n2i4.ea3mdgi7 > div > div > div > div > div > div.st-emotion-cache-0.e1f1d6gn0 > div > div > div.stHorizontalBlock.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.stColumn.st-emotion-cache-1b2d4l5.e1f1d6gn3 > div > div > div > div > div button:hover {
                     border-color: #00FF00;  /* Muda a cor da borda ao passar o mouse */
                 }
                 </style>
