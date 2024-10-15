b1276d259278360fa4fbf6488a208a82eadc91db
Renan Barbosa <renan_vianna7@icloud.com>
2024-10-14 23:06:59 -0300

    1 - Atualização na função de connection para obter do servidor correto.

--- UTILS/connection.py
+++ UTILS/connection.py
@@ -2,11 +2,12 @@ import mysql.connector
 
 
 def get_connection():
+    # Conectar ao MySQL
     connection = mysql.connector.connect(
-            host='geniusfut.c7k02g0my0as.us-east-2.rds.amazonaws.com',
-            user='renaneunao',
-            password='*Vitorya111',
-            database='geniusfut_database',
-            port=3306
-        )
+        host='geniusfut-2.c5y0u2k8gygo.us-east-1.rds.amazonaws.com',
+        user='renaneunao',
+        password='*Vitorya333',
+        database='geniusfut_database',
+        port=3306
+    )
     return connection
